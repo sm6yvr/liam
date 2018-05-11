65f100d9d399f4a4e20bf9408ba21ba0140a0ded
Ola Palm <ola.palm@sr.se>
2018-05-11 12:32:07 +0200

    changed sensors to 2 instead of 3. Default is 2?

--- Liam/Definition.h
+++ Liam/Definition.h
@@ -16,7 +16,7 @@
 #ifndef _DEFINITION_H_
 #define _DEFINITION_H_
 
-const int NUMBER_OF_SENSORS = 3;  // Number of BWF sensors can be 1-4 depending on shield
+const int NUMBER_OF_SENSORS = 2;  // Number of BWF sensors can be 1-4 depending on shield default 2 left and right front.
 
 // Pin setup following Morgan 1.5 shield and up
 #define WHEEL_MOTOR_A_CURRENT_PIN   0
